68012477ebfb65d64d47131efe36b168dc27d082
Jirayuth Porka <jambo5167@gmail.com>
2023-06-09 16:08:08 +0700

    added 49 return data script

--- Database_in_pandas/token_bitly.py
+++
@@ -1,6 +0,0 @@
-jirayuth_token = '2659b7dde7f007b1fe5cbf7784d78905800e3066'
-kitikun_token = '691e75c823d2856e59a28b26500bc7492654b85b'
-worawut_token = '7e16e4d609a04e7934263eb2e647f56b68baaefc'
-nui_token = 'ed048cf963484a750509d4afa94500aab248aa3e'
-
-list_token = [jirayuth_token, kitikun_token, worawut_token,nui_token]
